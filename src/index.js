48005438942e14d1f9dcc9ddd4f4af29ea80ec04
AntonioBaez13 <baezpinales.antonio@gmail.com>
2022-07-31 17:20:14 +0100

    Return all active todos that are on the database

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {TodoContainer} from './ListOfTodos';
 import AddTodo from './AddTodo';
 import TopBar from './TopBar';
 import TestersContainer from './ListOfTesters';
+import axios from 'axios';
 
 function App(props) {
+    const [isLoading, setLoading] = useState(true);
     const [todo, setTodo] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://localhost:5000/api/Todo/getactivetodos').then(response => {
+            setTodo(response.data);
+            setLoading(false);
+        });
+    }, []);
+
+    if(isLoading){
+        return (null);
+    }
+
     const addTodo = (x) => {
         setTodo([...todo, x]);
     };
+
     return (
         <div>
             <TopBar/>
